b7daf7e66c387192d00b511a8a5b75dd9c2f77c7
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-14 17:35:16 +0530

    C

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9999;
 
-app.get('./', (request, response) => {
+app.get('/', (request, response) => {
     response.send('Welcome to my nodejs app...');
     console.log('App is running...');
 });
