915d3cd7991a307b2d2add22b961c97207df2a15
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-14 17:26:29 +0530

    C

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Node demo app...");
+const express = require('express');
+const app = express();
+const port = 9999;
+
+app.get('./', (request, response) => {
+    response.send('Welcome to my nodejs app...');
+    console.log('App is running...');
+});
+
+app.listen(port, () => {
+    console.log('App is running on port ' + port);
+});
+
+
+
+
+
