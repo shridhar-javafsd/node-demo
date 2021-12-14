e8f5cbfea5bfdbc767858517f669f6fefe7e315e
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-14 17:39:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const port = 9999;
 
 app.get('/', (request, response) => {
     response.send('Welcome to my nodejs app...');
-    console.log('App is running...');
+    console.log('App...');
+});
+
+app.get('/home', (request, response) => {
+    response.send('Welcome to home...');
+    console.log('home...');
+});
+
+app.get('/contact', (request, response) => {
+    response.send('Contact me here...');
+    console.log('contact...');
 });
 
 app.listen(port, () => {
